62e0e31368eae2e2aededf5fbbdd1dae208f5da9
Esam-Ankir <eankir@hotmail.com>
2022-07-23 13:33:18 +0300

    add villas table

---
+++ src/models/villas/villas.js
@@ -0,0 +1,77 @@
+'use strict';
+const villasModel = (sequelize, DataTypes) => 
+sequelize.define('villas', {
+    userId: {
+        type: DataTypes.INTEGER,
+        required: true
+    },
+    process: {
+        type: DataTypes.ENUM('sell','rent'),
+        required: true
+    },
+    owner: {
+        type: DataTypes.STRING,
+        required: true
+
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        required: true
+    },
+    surfaceArea: {
+        type: DataTypes.FLOAT,
+        required: true
+    },
+    landArea: {
+        type: DataTypes.FLOAT,
+        required: true
+    },
+    floors: {
+        type: DataTypes.INTEGER,
+        required: true
+    },
+    buildingAge: {
+        type: DataTypes.ENUM('Under-Construction','0-11 months','1-5 years','6-9 years','10-19 years','+20 years'),
+        required: false
+    },
+    rooms: {
+        type: DataTypes.INTEGER,
+        required: false,
+    },
+    bathRooms: {
+        type: DataTypes.INTEGER,
+        required: false,
+    },
+
+    avilability: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+
+    furnished: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+
+    rentDuration: {
+        type: DataTypes.ENUM("Daily","Weekly","Monthly","Yearly"),
+        allowNull: false,
+    },
+
+    city: {
+        type: DataTypes.ENUM("Amman","Zarqa","Irbid","Aqaba","Mafraq","Jarash","Ma'an","Karak","AL-Salt","Ajloun","Tafilah","al-Balqa"),
+        allowNull: false,
+    },
+
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    moreInfo: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+});
+
+module.exports = villasModel;
